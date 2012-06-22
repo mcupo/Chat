1b1a6c59c2012e6fe533e3b478f35aff99d7bf0a
MCUPO <MCUPO@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-06-22 18:58:48 +0000

    estructura del TP4 + interfaz gráfica

---
+++ Java/T6/TP4/src/ar/edu/ort/client/Cliente.java
@@ -0,0 +1,420 @@
+package ar.edu.ort.client;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Enumeration;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import ar.edu.ort.common.Consts;
+import ar.edu.ort.common.Mensaje;
+
+public class Cliente extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	
+	private ClientThread clientThread;
+	private JTextField serverHost;
+	private JTextField serverPort;
+	private JTextField userNick;
+	private JTextField userMessage;
+	private JButton btnConnect;
+	private JButton btnDisconnect;
+	private JButton btnSend;
+	private JTextArea messageHistory;
+	private JList connectedUsers;
+	private DefaultListModel connectedUsersModel;
+
+
+	public Cliente()
+	{
+		init();
+	}
+	private int dialogoConectado()
+	{
+		Object[] options = {"Si", "No"};
+		int n = JOptionPane.showOptionDialog(this,
+		"Desea interrumpir la conexion a " + serverHost.getText() + " y cerrar la aplicación?",
+		"Cliente Chat - a2012c1 Grupo 3",
+		JOptionPane.YES_NO_OPTION,
+		JOptionPane.WARNING_MESSAGE,
+		null,
+		options,
+		options[1]);
+		return n;
+	}
+	
+	private void init()
+	{
+		setTitle("Grupo 3 - 3°2° B - Chat");
+		setBounds(0, 0, 720, 400);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	    //configurar acciones de la "x"
+		addWindowListener(new WindowAdapter()
+		{
+	        public void windowClosing(WindowEvent e)
+	        {
+	        	if (clientThread != null)
+	        	{
+	        		if (clientThread.isConnected())
+	        		{
+	        			int op = dialogoConectado();
+	        			switch (op)
+	        			{
+							case JOptionPane.NO_OPTION:break;
+							case JOptionPane.CLOSED_OPTION:break;
+							case JOptionPane.YES_OPTION:
+								System.exit(0);
+								break;
+							default:break;
+	        			}
+	        		}
+	        		else
+	        		{
+	        			System.exit(0);
+	        		}
+	        	}
+	        	else
+	        	{
+	        		System.exit(0);
+	        	}
+	        }
+	    });
+		getContentPane().setLayout(new BorderLayout());
+
+		JPanel pnl, pnlAux;
+		JLabel lbl;
+		
+		//
+		// TOP Panel - Datos del Servidor
+		//
+		pnl = new JPanel();
+		pnl.setLayout(null);
+		pnl.setBackground(Color.LIGHT_GRAY);
+		pnl.setPreferredSize(new Dimension(0, 34));
+		getContentPane().add(pnl, BorderLayout.NORTH);
+
+		lbl = new JLabel("Host:");
+		pnl.add(lbl);
+		lbl.setBounds(8, 8, 32, 22);
+		serverHost = new JTextField("127.0.0.1");
+		pnl.add(serverHost);
+		serverHost.setBounds(40, 8, 100, 22);
+
+		lbl = new JLabel("Port:");
+		pnl.add(lbl);
+		lbl.setBounds(160, 8, 32, 22);
+		serverPort = new JTextField(String.valueOf(Consts.DEFAULT_SEVER_PORT));
+		pnl.add(serverPort);
+		serverPort.setBounds(192, 8, 100, 22);
+
+		lbl = new JLabel("Nick:");
+		pnl.add(lbl);
+		lbl.setBounds(312, 8, 32, 22);
+		userNick = new JTextField("mi nick");
+		pnl.add(userNick);
+		userNick.setBounds(344, 8, 100, 22);
+		
+		btnConnect = new JButton("Conectar");
+		pnl.add(btnConnect);
+		btnConnect.setBounds(450, 8, 120, 22);
+		btnConnect.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				clientStart();
+			}
+		});
+		btnDisconnect = new JButton("Desconectar");
+		btnDisconnect.setEnabled(false);
+		pnl.add(btnDisconnect);
+		btnDisconnect.setBounds(578, 8, 120, 22);
+		btnDisconnect.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				clientStop();
+			}
+		});
+		
+		//
+		// JList - Nicks de los usuarios logueados
+		//
+		pnlAux = new JPanel();
+		pnlAux.setLayout(new BorderLayout());
+		pnlAux.setBackground(Color.RED);
+		pnlAux.setPreferredSize(new Dimension(200, 0));
+		getContentPane().add(pnlAux, BorderLayout.EAST);
+		
+		lbl = new JLabel("Usuarios conectados:");
+		pnlAux.add(lbl, BorderLayout.NORTH);
+		connectedUsersModel = new DefaultListModel();
+		connectedUsers = new JList(connectedUsersModel);
+		connectedUsers.setPreferredSize(new Dimension(200, 0));
+		connectedUsers.setBackground(Color.ORANGE);
+		pnlAux.add(connectedUsers, BorderLayout.CENTER);
+
+		//
+		// CENTER Panel - Panel con datos de mensajes
+		//
+		pnl = new JPanel();
+		pnl.setLayout(new BorderLayout());
+		pnl.setBackground(Color.GRAY);
+		getContentPane().add(pnl, BorderLayout.CENTER);
+
+		lbl = new JLabel("Historial de mensajes:");
+		pnl.add(lbl, BorderLayout.NORTH);
+		lbl.setForeground(Color.WHITE);
+		messageHistory = new JTextArea();
+		messageHistory.setBackground(Color.WHITE);
+		messageHistory.setEditable(false);
+		pnl.add(messageHistory, BorderLayout.CENTER);
+		
+		//
+		// CENTER Panel - Area para escribir mensajes
+		// está localizado al SOUTH del CENTER Panel.
+		//
+		pnlAux = new JPanel();
+		pnlAux.setLayout(null);
+		pnlAux.setBackground(Color.GRAY);
+		pnlAux.setPreferredSize(new Dimension(0, 34));
+		pnl.add(pnlAux, BorderLayout.SOUTH);
+		
+		lbl = new JLabel("Texto:");
+		pnlAux.add(lbl);
+		lbl.setBounds(8, 8, 50, 22);
+		userMessage = new JTextField();
+		userMessage.setEditable(false);
+		pnlAux.add(userMessage);
+		userMessage.setBounds(60, 8, 300, 22);
+		
+		btnSend = new JButton("Enviar");
+		btnSend.setEnabled(false);
+		pnlAux.add(btnSend);
+		btnSend.setBounds(380, 8, 120, 22);
+		btnSend.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				clientSend();
+			}
+		});
+	}
+	
+	private void clientStart()
+	{
+			clientThread = new ClientThread();
+			clientThread.start();
+			//actualizo la vista
+			serverHost.setEditable(false);
+			serverPort.setEditable(false);
+			userNick.setEditable(false);
+			btnConnect.setEnabled(false);
+			btnDisconnect.setEnabled(true);
+	}
+	
+	private void clientStop()
+	{
+		if (clientThread != null)
+		{
+			clientThread.terminate();
+			clientThread = null;
+			//actualizo la vista
+			serverHost.setEditable(true);
+			serverPort.setEditable(true);
+			userNick.setEditable(true);
+			btnConnect.setEnabled(true);
+			btnDisconnect.setEnabled(false);
+			btnSend.setEnabled(false);
+			connectedUsersModel.removeAllElements();
+			userMessage.setText("");
+			userMessage.setEditable(false);
+		}
+	}
+	
+	private void clientSend()
+	{
+		if (clientThread != null)
+		{
+			clientThread.sendText();
+		}
+	}
+	
+	private void clientError(String msg)
+	{
+		JOptionPane.showMessageDialog(this, msg);
+	}
+	private void agregarUsuario(String usr)
+	{
+		connectedUsersModel.addElement(usr);
+	}
+	private void removerUsuario(String usr)
+	{
+		connectedUsersModel.removeElement(usr);
+	}
+	public static void main(String[] args)
+	{
+		Cliente cliente = new Cliente();
+		cliente.setVisible(true);
+	}
+	
+	class ClientThread extends Thread
+	{
+		private Socket skt;
+		private ObjectOutputStream oos;
+		private boolean connected;
+		
+		public void run()
+		{
+			connected=false;
+			try
+			{
+				skt = new Socket(serverHost.getText(), Integer.parseInt(serverPort.getText()));
+				try
+				{
+					oos = new ObjectOutputStream(skt.getOutputStream());
+					try
+					{
+						//
+						// Enviar LOGIN
+						//
+						try
+						{
+							
+							
+							
+				            messageHistory.setText( "Iniciando conexión a " + serverHost.getText()+":"+ Integer.parseInt(serverPort.getText()));
+							System.out.println(userNick.getText().trim()+":login enviado");
+						} 
+						//catch(IOException e)
+						catch(Exception e)
+						{
+							clientError("Error en la conexión.");
+						}
+						ObjectInputStream ois = new ObjectInputStream(skt.getInputStream());						
+						// Quedarse en loop hasta que se corte
+						while (skt.isConnected())
+						{
+							try
+							{
+								Mensaje msg = (Mensaje) ois.readObject();
+								System.out.println(userNick.getText().trim()+":mensaje recibido, tipo:" + String.valueOf(msg.getType()));
+								procesarMensaje(msg);
+							}
+							catch(ClassNotFoundException e)
+							{
+								clientError(e.getMessage());
+								clientStop();
+								break;
+							}
+							catch (IOException e)
+							{
+								if (isConnected())
+								{
+									clientError("Error en la conexión.");
+								}
+								clientStop();
+								break;								
+							}
+						}
+					}
+					catch
+					(IOException e)
+					{
+						clientError("Error en la conexión.");
+						clientStop();
+					}
+				}
+				catch(IOException e)
+				{
+					clientError("Error en la conexión.");
+					clientStop();
+				}
+			}
+			catch(IOException e)
+			{
+				clientError("Error al conectarse a " + serverHost.getText()+":"+serverPort.getText());
+				clientStop();
+			}
+		}
+		
+		public void terminate()
+		{
+			if (skt != null)
+			{
+				connected=false;
+				try
+				{
+					//El logout lo realiza el server automaticamente ante el cierre del socket
+					skt.close();
+				}
+				catch(IOException e){}
+			}
+		}
+		
+		public void sendText()
+		{
+			if (skt != null)
+			{
+				try
+				{
+					Mensaje msg = new Mensaje();
+					msg.setType(Mensaje.MESSAGE);
+
+					//agregar lista de usuarios seleccionados como destinatarios
+					Object[] users=connectedUsers.getSelectedValues();
+					for (int i=0;i<users.length;i++)
+					{
+						if (!users[i].toString().equals(userNick.getText()))
+							msg.getUsers().add(users[i]);						
+					}
+					oos.writeObject(msg);
+				}
+				catch(IOException e){}
+			}
+		}
+		
+		public void procesarMensaje(Mensaje msg)
+		{
+			 String mostrar = null;
+			 System.out.println("Procesando Mensaje");
+		     switch( msg.getType() )
+		     {
+		       case Mensaje.LOGOUT:
+		               break;
+		       case Mensaje.LOGIN:
+		               break;
+		       case Mensaje.MESSAGE:
+		               break;
+		       case Mensaje.LIST:
+		               break;                        
+		       case Mensaje.ERROR:
+		               clientError("El nickname " + msg.getNick() + " ya se encuentra en uso. Ingrese uno nuevo.");
+		               clientStop();
+		               break;                                        
+		       default: return;
+		     }
+		}
+		
+		public boolean isConnected()
+		{
+			return connected;
+		}
+	}
+}
---
+++ Java/T6/TP4/src/ar/edu/ort/server/Servidor.java
@@ -0,0 +1,294 @@
+package ar.edu.ort.server;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import ar.edu.ort.common.Consts;
+import ar.edu.ort.common.Mensaje;
+
+public final class Servidor extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	JButton btnIniciar;
+	JButton btnDetener;
+	
+	private ServerThread serverThread;
+	
+	public Servidor()
+	{
+		init();
+	}
+	
+	private void init() 
+	{
+		setTitle("Grupo XX - Puerto:" + Consts.DEFAULT_SEVER_PORT);
+		setBounds(0, 0, 160, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new FlowLayout());
+		
+		btnIniciar = new JButton("Iniciar servidor ");
+		getContentPane().add(btnIniciar);
+		btnIniciar.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				serverStart();
+				btnIniciar.setEnabled(false);
+				btnDetener.setEnabled(true);
+				
+			}});
+		btnDetener = new JButton("Detener servidor");
+		btnDetener.setEnabled(false);
+		getContentPane().add(btnDetener);
+		btnDetener.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				serverStop();
+				btnIniciar.setEnabled(true);
+				btnDetener.setEnabled(false);				
+			}});
+	}
+	
+	private void serverStart()
+	{
+		if (serverThread == null)
+		{
+			serverThread = new ServerThread();
+			serverThread.start();
+		}
+	}
+	
+	private void serverStop()
+	{
+		if (serverThread != null)
+		{
+			serverThread.terminate();
+			serverThread = null;
+		}
+	}
+	
+	private void serverError(String msg)
+	{
+		JOptionPane.showMessageDialog(this, msg);
+	}
+
+	public static void main(String[] args)
+	{
+		Servidor servidor = new Servidor();
+		servidor.setVisible(true);
+	}
+	
+	class ClientWorker extends Observable implements Runnable
+	{
+		private Socket cliente;
+		ObjectInputStream in;
+		ObjectOutputStream out;
+		private String nick;
+		  
+		ClientWorker(Socket cliente)
+		{
+			  this.cliente = cliente;
+			  nick="";
+		}
+
+		public void run()
+		{
+			try
+			{
+			  in = new ObjectInputStream(cliente.getInputStream());
+			  out = new ObjectOutputStream(cliente.getOutputStream());
+			}
+			catch(IOException e)
+			{
+				serverError(e.getMessage());
+			}
+			//Se queda leyendo mensajes del cliente
+			while(!cliente.isInputShutdown())
+			{
+				try
+				{
+					Mensaje msg = (Mensaje) in.readObject();
+					setChanged();
+					notifyObservers(msg);
+			    }
+				catch(IOException e)
+				{
+					//serverError(e.getMessage());
+					break;
+			    }
+				catch(ClassNotFoundException e)
+				{
+					e.printStackTrace();
+			    }
+			}
+			System.out.println("Error en la conexion de " + nick);
+			terminate();
+		  }
+
+		public boolean isLoggedIn()
+		{
+			return (!nick.equals(""));
+		}
+
+	
+		//Enviar Mensaje 		
+		public void enviarMensaje(Mensaje msg)
+		{
+	        try
+	        {
+	            this.out.writeObject(msg);
+	        }
+	        catch(IOException io)
+	        {
+	            System.out.println("Error al intentar enviar msg al usuario " + msg.getNick());
+	        }            
+	    }
+		
+		public void terminate()
+		{
+			try
+			{
+				in.close();
+				out.close();
+				cliente.close();
+				if (isLoggedIn()){}
+			}
+			catch(IOException e)
+			{
+				serverError(e.getMessage());
+			}
+		}
+
+		public String getNick()
+		{
+			return nick;
+		}
+
+		public void setNick(String nick)
+		{
+			this.nick = nick;
+		}
+	} //Fin ClientWorker
+
+	
+	class ServerThread extends Thread implements Observer
+	{
+		private ServerSocket ss;
+		private Hashtable listaNicks;
+		
+		public ServerThread()
+		{
+			// La hashtable guarda como Key el nick del usuario
+			// y como Element el ObjectOutputStream.
+			listaNicks = new Hashtable();
+		}
+		
+		public void run()
+		{
+			try
+			{
+				ss = new ServerSocket(Consts.DEFAULT_SEVER_PORT);
+				while (!ss.isClosed())
+				{
+					Socket skt = ss.accept();
+					// Lanza hilo para atender al cliente que se conectó
+					ClientWorker cw =new ClientWorker(skt);
+					cw.addObserver(this);
+			        Thread t = new Thread(cw);
+			        t.start();
+				}
+				
+			}
+			catch (IOException e)
+			{
+				serverError(e.getMessage());
+			}
+			finally
+			{
+				ss = null;
+			}
+		}
+		public void terminate()
+		{
+			if (ss != null)
+			{
+				try
+				{
+					Enumeration lista = listaNicks.keys();
+			    	while (lista.hasMoreElements()){
+			    		ClientWorker cw = (ClientWorker) listaNicks.get(lista.nextElement());
+			    		cw.setNick("");
+			    		cw.terminate();
+			    	}
+					ss.close();
+				}
+				catch(IOException e){}
+			}
+		}
+		public void update(Observable who, Object what)
+		{
+			Mensaje msg = (Mensaje) what;
+			System.out.println(msg.getNick()+ ":mensaje recibido, tipo:" + String.valueOf(msg.getType()));
+			ClientWorker cw = (ClientWorker) who;
+			//procesa el mensaje
+	        switch (msg.getType())
+	        {
+            case Mensaje.LOGOUT:
+                break;
+            case Mensaje.LOGIN:	                
+                break;
+            case Mensaje.MESSAGE:
+                break;
+	        }
+		}
+		
+	    //Agregar usuarios
+	    private void agregarUsuario(String nick, ClientWorker cw)
+	    {
+	        listaNicks.put(nick, cw);
+	    }
+
+	    //Verificacion de Nick
+	    private boolean nickExiste(String nickName)
+	    {
+	        return listaNicks.containsKey(nickName);
+	    }
+	    
+	    //Quitar usuario
+	    private void quitarUsuario(String nick)
+	    {
+	        listaNicks.remove(nick);
+	    }
+	    
+	    private Vector getListaConectados()
+	    {
+	    	Vector vec = new Vector();
+	    	Enumeration lista = listaNicks.keys();
+	    	while (lista.hasMoreElements())
+	    	{
+	    		vec.addElement(lista.nextElement());
+	    	}
+	    	return vec;
+	    }
+	    
+	    private void propagarMensaje(Mensaje msg)
+	    {
+	    }
+	} //Fin ServerThread
+}
